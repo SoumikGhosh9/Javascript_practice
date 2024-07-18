8b90822fdb3abdb6c4e99574ddc02e45cad1cc9f
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-07-18 16:15:40 +0000

    Control Structures

--- task3.js
+++ task3.js
@@ -41,3 +41,62 @@ else{
     }
     
 console.log("Largest is",largest);
+
+//4. Write a program that uses switch case to determine the  day of the week based on a number (1-7) and log the day name to the console
+let day = 2;
+let dayname;
+switch (day) {
+    case 1:
+        dayname = "Sunday"
+        break;
+    case 2:
+        dayname = "Monday"
+        break;
+    case 3:
+        dayname = "Tuesday"
+        break;
+    case 4:
+        dayname = "Wednesday"
+        break;
+    case 5:
+        dayname = "Thursday"
+        break;
+    case 6:
+        dayname = "Friday"
+        break;
+    case 7:
+        dayname = "Saturday"
+        break;
+
+    default:
+        dayname = "Wrong Input"
+        break;
+}
+console.log("Today is "+dayname)
+
+//5. Write a program that uses switch case to assign a grade ('A','B','C','D','E') based on a score and log the grade to the console
+let number = 75;
+let grade;
+switch (true) {
+    case (number>=90):
+        grade = "A";
+        break;
+    case (number>=80):
+        grade = "B";
+        break;
+    case (number>=70):
+        grade = "C";
+        break;
+    case (number>=60):
+        grade = "D";
+        break;
+    case (number>=50):
+        grade = "E";
+        break;
+
+    default:
+        grade = "F";
+        break;
+}
+console.log("Grade is"+grade);
+
