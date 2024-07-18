ef2410ddfc552bc99f8aede3d10cb9e0b8ce37bb
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-07-18 15:38:29 +0000

    Control Structures

---
+++ task3.js
@@ -0,0 +1,20 @@
+//1. Write a program to check a number is positive negative or zero and the result to the console
+const num1 = 12
+if (num1>0) {
+    console.log("Number is positive");
+}
+else if (num1==0) {
+    console.log("Number is zero")
+} 
+else {
+    console.log("Number is negative")
+}
+
+//2. Write a program to check if a number is eligible to vote or not and log to the console
+const age = 12
+if (age>=18) {
+    console.log("You are eligible to vote");
+}
+else{
+    console.log("You are not eligible to vote");
+}
