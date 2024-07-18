4459c02faab4f8e18c645ec187b8cf2897bf669c
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-07-18 15:54:38 +0000

    Control Structures

--- task3.js
+++ task3.js
@@ -18,3 +18,26 @@ if (age>=18) {
 else{
     console.log("You are not eligible to vote");
 }
+
+//3. Write a program to find the largest of three numbers using nested if else statement
+    let largest;
+    const n1 = 15
+    const n2 = 5
+    const n3 = 8
+    
+    
+    if (n1 >= n2) {
+        if (n1 >= n3) {
+            largest = n1;
+        } else {
+            largest = n3;
+        }
+    } else {
+        if (n2 >= n3) {
+            largest = n2;
+        } else {
+            largest = n3;
+        }
+    }
+    
+console.log("Largest is",largest);
