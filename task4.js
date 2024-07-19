5eea85161c6a56a31fa93e751eeb19434b8f1a9e
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-07-19 15:49:40 +0000

    Loops

--- task4.js
+++ task4.js
@@ -1,13 +1,27 @@
-// 1. Write a program to print 1 to 10 numbers using for loop and log to the console
-for ( let i = 1; i <=10; i++){
-    console.log(i);
+// // 1. Write a program to print 1 to 10 numbers using for loop and log to the console
+// for ( let i = 1; i <=10; i++){
+//     console.log(i);
    
-}
+// }
 
-// 2. Write a program to print the multiplication table of 5 using for loop
-let a = 5;
-for(let i =1;i <=10; i++){
-    let sum = a*i;
-    console.log(sum);
+// // 2. Write a program to print the multiplication table of 5 using for loop
+// let a = 5;
+// for(let i =1;i <=10; i++){
+//     let sum = a*i;
+//     console.log(sum);
+// }
+
+// // 3. Write a program to calculate the sum of numbers 1 to 10 using for loops
+// let s = 0;
+// for(let i = 0; i <=10; i++){
+//      s += i;
+//     console.log(s)
+// }
+
+// 4. Write a program to print numbers from 10 to 1 using while loop
+let i = 10;
+while (i>=1) {
+    console.log(i);
+    i--;
 }
 
