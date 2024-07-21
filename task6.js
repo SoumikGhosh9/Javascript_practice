31f3c225301371e38a63b65af0c621bbc217b75f
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-07-21 17:34:36 +0000

    Arrays

---
+++ task6.js
@@ -0,0 +1,8 @@
+// 1. Create an array from numbers 1 to 5 and log the result to the console
+let numbers = [1,2,3,4,5]
+console.log(numbers);
+
+// 2. Access the first element and last elements of an array and log them to the console
+let fruits = ["apple", "banana", "mango"]
+console.log(fruits[0]);
+console.log(fruits[2]);
