4ddb7724146e954a590731e282e6712a4764e476
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-08-21 16:36:32 +0000

    DatatypeConversion

--- Datatypesconversion.js
+++ Datatypesconversion.js
@@ -16,3 +16,4 @@ console.log("1"+2 + 2);
 
 //If String is at first then all the elements will be treated as string
 //If String is not at first then which element is a string that element will be treated as a string
+
