c426cb92179bee0a08798ceda5f60d3665513609
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-08-21 14:57:01 +0000

    Datatypes conversion

---
+++ Datatypesconversion.js
@@ -0,0 +1,18 @@
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " Soumik"
+let str3 = str1 + str2;
+console.log(str3);
+
+// Conversion of strings
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log(1 + 2 + "2");
+console.log("1"+2 + 2);
+
+//If String is at first then all the elements will be treated as string
+//If String is not at first then which element is a string that element will be treated as a string
