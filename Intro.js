f9cb486958a53858f764f4b2575f8fe7cc879367
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-08-24 14:23:23 +0000

    Introduction

--- Intro.js
+++ Intro.js
@@ -1,7 +1,11 @@
 // For creating the alert popup in Javascript
 
-alert('Good Morning!!')
+// alert('Good Morning!!')
 
 //Change a web page to display "I am Learning JS"
 
-document.body.innerHTML = "I am Learning JS"
+// document.body.innerHTML = "I am Learning JS"
+
+console.log("Good morning");
+console.warn("This is a warning");
+console.error("There is an error");
