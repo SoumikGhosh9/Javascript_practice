d6023850d57226bd91be5cdeb92f9175b420cbc0
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-08-31 07:00:59 +0000

    Objects

--- task7.js
+++ task7.js
@@ -3,7 +3,18 @@ let product = {
     price: 899,
     item_name: 'T_Shirt'
 }
+// console.log(product);
+// console.log(product.company);
+// console.log(product.item_name);
+// console.log(product.price);
+
+product.company = 'banana';
 console.log(product);
-console.log(product.company);
-console.log(product.item_name);
-console.log(product.price);
+console.log(product['company']);
+console.log(product['item_name']);
+console.log(product['price']);
+
+
+delete product.company;
+console.log(product);
+
