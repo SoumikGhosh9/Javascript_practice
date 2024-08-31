e12676262435c43a0e222707b127d9c1fd5a8c34
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-08-31 06:42:04 +0000

    Operators

--- task7.js
+++ task7.js
@@ -1,1 +1,9 @@
-7
+let product = {
+    company : 'Mango',
+    price: 899,
+    item_name: 'T_Shirt'
+}
+console.log(product);
+console.log(product.company);
+console.log(product.item_name);
+console.log(product.price);
