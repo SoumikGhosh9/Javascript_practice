85d11f2c4959551795c15569cb1e582c85a71375
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-08-31 07:12:18 +0000

    Objects

--- task7.js
+++ task7.js
@@ -1,7 +1,10 @@
 let product = {
     company : 'Mango',
     price: 899,
-    item_name: 'T_Shirt'
+    item_name: 'T_Shirt',
+    displayPrice :function(){
+        console.log(`The price of the product is ${this.price}`)
+    }
 }
 // console.log(product);
 // console.log(product.company);
@@ -17,4 +20,13 @@ console.log(product['price']);
 
 delete product.company;
 console.log(product);
+
+product.displayPrice();
+
+
+//AutoBoxing
+
+console.log("Hey this is Soumik".length);
+console.log("Hey this is Soumik".toUpperCase());
+console.log("Hey this is Soumik".replace('this is','I am'));
 
